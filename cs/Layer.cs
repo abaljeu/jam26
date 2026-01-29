41ef2fe80a37475f54d3abb2cdbb703e6a031905
Alan Baljeu <alanbaljeu@yahoo.com>
2026-01-28 22:25:42 -0500

    Expand game model.

--- cs/Layer.cs
+++ cs/Layer.cs
@@ -2,9 +2,34 @@
 {
   public class Layer
   {
-    static Layer FromWorld(int level, Mask[] masks)
+    private Tile[,] tiles;
+
+    public static Layer FromWorld(int level, Mask[] masks)
     {
       return new Layer();
+    }
+    public static ETile PickTile(int i, int j)
+    {
+      if (i == 18 && j == 16)
+        return ETile.Ladder;
+      if (Math.Abs(i - j) > 10)
+        return ETile.Rock;
+      if (Math.Abs(i + j) < 25)
+        return ETile.Dirt;
+      return ETile.Space;
+    }
+    public static Layer ExampleLayer()
+    {
+      Layer lay = new Layer();
+      lay.tiles = new Tile[20, 20];
+      for (int i = 0; i < 20; i++)
+      {
+        for (int j = 0; j < 20; j++)
+        {
+          lay.tiles[i, j] = new Tile(PickTile(i, j));
+        }
+      }
+      return lay;
     }
   }
 }
