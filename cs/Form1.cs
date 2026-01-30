006c1b68c84fc1b92260ed771f0b395846a8b007
clandrew <cmlandrews@gmail.com>
2026-01-30 18:43:15 -0500

    Drawing happens with an intermediate

--- cs/Form1.cs
+++ cs/Form1.cs
@@ -1,14 +1,18 @@
 
+using System.Drawing.Drawing2D;
+
 namespace mask
 {
     public partial class Form1 : Form
     {
         Bitmap reference;
+        Bitmap native;
 
         public Form1()
         {
             InitializeComponent();
             reference = new Bitmap("D:\\repos\\jam26\\images\\Reference.png");
+            native = new Bitmap(320, 240);
         }
 
         private void gdiControl1_MouseMove(object sender, MouseEventArgs e)
@@ -28,8 +32,15 @@ namespace mask
 
         private void gdiControl1_Paint(object sender, PaintEventArgs e)
         {
+            {
+                Graphics g = Graphics.FromImage(native);
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+                g.DrawImageUnscaled(reference, 0, 0);
+            }
+
+            // Draw native to final target
             e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
-            e.Graphics.DrawImageUnscaled(reference, 0, 0);
+            e.Graphics.DrawImageUnscaled(native, 0, 0);
         }
     }
 }
