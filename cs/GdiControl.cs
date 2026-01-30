becd9f39f415a94949fd3779521acef9b489caaf
clandrew <cmlandrews@gmail.com>
2026-01-30 18:57:48 -0500

    Fix the scaling

--- cs/GdiControl.cs
+++ cs/GdiControl.cs
@@ -5,35 +5,40 @@ using System.Windows.Forms;
 //tremnonal
 namespace mask
 {
-  // idea: class Tile.  draws itself and border, using bitmap image.  maybe applies mask filter, or maybe image does and we use it.
-  public class GdiControl : Control
-  {
-    Bitmap reference;
-    Bitmap native;
-    
-    public GdiControl()
+    // idea: class Tile.  draws itself and border, using bitmap image.  maybe applies mask filter, or maybe image does and we use it.
+    public class GdiControl : Control
     {
-        reference = new Bitmap("D:\\repos\\jam26\\images\\Reference.png");
-        native = new Bitmap(320, 240);
-    }
+        Bitmap reference;
+        Bitmap native;
 
-    protected override void OnPaint(PaintEventArgs e)
-    {
+        public GdiControl()
         {
-            Graphics g = Graphics.FromImage(native);
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
-            g.DrawImageUnscaled(reference, 0, 0);
+            reference = new Bitmap("D:\\repos\\jam26\\images\\Reference.png");
+            reference.SetResolution(96, 96);
+
+            native = new Bitmap(320, 240);
+            native.SetResolution(96, 96);
         }
 
-        // Draw native to final target
-        e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
-        e.Graphics.DrawImageUnscaled(native, 0, 0);
-    }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            {
+                Graphics g = Graphics.FromImage(native);
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+                g.DrawImageUnscaled(reference, 0, 0, 320, 240);
+            }
 
-  }
+            // Draw native to final target with 2x scaling
+            int scaleFactor = 3;
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
+            e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
+            e.Graphics.DrawImage(native, 0, 0, 320 * scaleFactor, 240 * scaleFactor);
+        }
+
+    }
 }
 
 
 
 
-  
